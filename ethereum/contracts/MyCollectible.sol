b522e4003742c998683fdc7afaebbd381768b055
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2021-02-14 16:02:27 +0200

    functionality to mint NFT when burning full tokens

--- ethereum/contracts/MyCollectible.sol
+++ ethereum/contracts/MyCollectible.sol
@@ -3,6 +3,14 @@ pragma solidity ^0.6.0;
 import "./token/ERC721/ERC721.sol";
 
 contract MyCollectible is ERC721 {
-    constructor() ERC721("MyCollectible", "MCO") public {
+    address private _minter;
+
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) public {
+        _minter = msg.sender;
+    }
+
+    function mintCollectible(address receiver, uint tokenId) public {
+        require(msg.sender == _minter, "only minter can mint");
+        _safeMint(receiver, tokenId);
     }
 }
--- ethereum/contracts/MyToken.sol
+++ ethereum/contracts/MyToken.sol
@@ -2,10 +2,32 @@ pragma solidity ^0.6.0;
 
 import "./token/ERC20/ERC20.sol";
 import "./token/ERC20/ERC20Burnable.sol";
+import "./MyCollectible.sol";
 
 contract MyToken is ERC20Burnable {
+    MyCollectible public col;
+
     constructor(uint initialSupply, string memory name, string memory symbol) 
       ERC20(name, symbol) public {
           _mint(msg.sender, initialSupply);
+          col = new MyCollectible("MyCollectible", "MCO");
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from the caller and mints an NFT
+     *
+     * See {ERC20-_burn}.
+     */
+    function burn(uint256 amount) override public virtual {
+        _burn(msg.sender, amount);
+        // If burning at least one full token, mint an NFT for each full token
+        if (amount >= 10 ** 18) {
+            uint fullTokens = amount / (10 ** 18);
+            for (uint i = 0; i < fullTokens; i++) {
+                uint newId = col.totalSupply() + 1;
+                col.mintCollectible(msg.sender, newId);
+            }
+            
+        }
     }
 }
