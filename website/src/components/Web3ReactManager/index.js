ff78616323022658867f1eac7ae47e43ac498538
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2021-02-16 20:22:49 +0200

    dotenv to use vars

--- website/src/components/Web3ReactManager/index.js
+++ website/src/components/Web3ReactManager/index.js
@@ -6,7 +6,6 @@ import { USED_CHAIN_ID } from '../../utils'
 
 export default function Web3ReactManager({ children }) {
   const { setConnector, error, active, networkId, connector } = useWeb3Context()
-  console.log('error is?', error)
 
   const checkCorrectNetwork = async (library) => {
     var network = await library.getNetwork();
@@ -16,8 +15,9 @@ export default function Web3ReactManager({ children }) {
     }
     return true;
   }
-  /*
- useEffect(()  =>  {
+
+  // initialization management
+  useEffect(()  =>  {
     const main = async () => {
       if (!active) {
         if (window.ethereum) {
@@ -44,35 +44,6 @@ export default function Web3ReactManager({ children }) {
       }
     }
    main();
-  }, [active, setConnector])
-  */
-
-  // initialization management
-  useEffect(() => {
-    if (!active) {
-      if (window.ethereum) {
-        try {
-          const library = new ethers.providers.Web3Provider(window.ethereum)
-          console.log('found library', library);
-          library.listAccounts().then(accounts => {
-            console.log('found accounts', accounts);
-            if (accounts.length >= 1) {
-              console.log('found acc1');
-              setConnector('Injected', { suppressAndThrowErrors: true })
-            } else {
-              console.log('found acc15');
-              setConnector('Network')
-            }
-          })
-        } catch (e) {
-          console.log('found errr', e);
-          setConnector('Network')
-        }
-      } else {
-        console.log('found hdddd');
-        setConnector('Network')
-      }
-    }
   }, [active, setConnector])
 
   const [showLoader, setShowLoader] = useState(false)
