3061b44cdf9539f8a0145a287082bb1735f88984
LauriPeltonen <lauri.peltonen@zure.com>
2021-03-06 10:40:12 +0200

    f

--- website/src/pages/Main/index.tsx
+++ website/src/pages/Main/index.tsx
@@ -246,9 +246,9 @@ export default function Main({showStats, showLearnMore, showFAQ} : Props) {
     try {
       const getPrice = async () => {
         const key = process.env.REACT_APP_ETHERSCAN_API_KEY_PRICE;
-        const url = '.netlify/functions/ethprice'
+        const url = 'https://dpricebranding.netlify.app/.netlify/functions/ethprice';//'.netlify/functions/ethprice'
         const res = await fetch(url);
-        console.log('res', res)
+        console.log('res', res.json())
       }
       
       getPrice();
